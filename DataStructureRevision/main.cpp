e7046da61b337c278f94837142edb02c75d5ac19
Evian-Zhang <evianzhang1999@163.com>
2018-12-28 23:43:14 +0800

    Initial Commit

---
+++ DataStructureRevision/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructureRevision
+//
+//  Created by Evian张 on 2018/12/28.
+//  Copyright © 2018 Evian张. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
