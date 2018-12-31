a2c008f668b2ee9aa4e0309cbc82c4611ed27924
Evian-Zhang <evianzhang1999@163.com>
2018-12-31 12:29:07 +0800

    add isEmpty()

--- DataStructureRevision/Queue/CircularQueue.hpp
+++ DataStructureRevision/Queue/CircularQueue.hpp
@@ -46,6 +46,9 @@ public:
      @param value 传出的队首值
      */
     bool getFront(T& value);
+    
+    /** 判断队列内是否有元素 */
+    bool isEmpty();
 };
 
 template <typename T>
@@ -113,6 +116,12 @@ bool CircularQueue<T>::getFront(T &value)
         return false;
     value = this->list[this->frontIndex];
     return true;
+}
+
+template <typename T>
+bool CircularQueue<T>::isEmpty()
+{
+    return this->count == 0;
 }
 
 #endif /* Queue_hpp */
